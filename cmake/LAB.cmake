7d795edbd6af2a24e7fc15643296649adc9e1dd1
CLMC-Lab User <sschaal@usc.edu>
2014-04-07 08:00:40 -0700

    added a debugging statement to print out all cmake variables if uncommented

--- cmake/LAB.cmake
+++ cmake/LAB.cmake
@@ -128,6 +128,9 @@ set(_variableNames
 		   MY_BINDIR 	
 		   DOCUMENTATIONDIR )
 
+# only uncomment for debugging all cmake variables
+# get_cmake_property(_variableNames VARIABLES)
+
 foreach (_variableName ${_variableNames})
     message(STATUS "${_variableName}=${${_variableName}}")
 endforeach()
