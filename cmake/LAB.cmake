3f897eb456ee304ac2494ac0138752f9ecf8c765
CLMC-Lab User <sschaal@usc.edu>
2014-04-09 06:40:13 -0700

    cmake fixes

--- cmake/LAB.cmake
+++ cmake/LAB.cmake
@@ -66,7 +66,6 @@ if ($ENV{MACHTYPE} STREQUAL "x86_64mac")
   include_directories(BEFORE /usr/X11/include /opt/local/include /sw/include /usr/include)
   link_directories(/sw/lib /usr/X11/lib /opt/local/lib /usr/lib ${CMAKE_LIBRARY_PATH})
   set(LAB_STD_LIBS edit curses glut GL GLU X11 m)
-  set(CMAKE_C_FLAGS "-fnested-functions ${CMAKE_C_FLAGS}")
 
 # check for problematic compiler flags
   include(CheckCCompilerFlag)
