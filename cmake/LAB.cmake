b20ff86551b9332bfbdef108a4a4da3631b3cc4a
Jeannette Bohg <jbohg@tuebingen.mpg.de>
2015-07-01 12:19:47 +0200

    removing stdc++ from LAB_STD_LIBRARIES

--- cmake/LAB.cmake
+++ cmake/LAB.cmake
@@ -82,7 +82,7 @@ elseif ($ENV{MACHTYPE} STREQUAL "x86_64xeno" )
   						  OUTPUT_VARIABLE XENOMAI_LD_FLAGS)  
   set(CMAKE_C_FLAGS "${XENOMAI_C_FLAGS} ${CMAKE_C_FLAGS}")
   set(CMAKE_CPP_FLAGS "${XENOMAI_C_FLAGS} ${CMAKE_CPP_FLAGS}")
-  set(LAB_STD_LIBS native xenomai pthread rtdk analogy rtdm edit curses nsl glut GL GLU X11 Xmu m stdc++)
+  set(LAB_STD_LIBS native xenomai pthread rtdk analogy rtdm edit curses nsl glut GL GLU X11 Xmu m)
 
   link_directories(${XENOMAI_ROOT}/lib /usr/X11/lib64 /usr/X11/lib /usr/lib64 ${CMAKE_LIBRARY_PATH})
   include_directories(BEFORE ${XENOMAI_C_FLAGS})
